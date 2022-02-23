3bf0a9521cdb33935c2f4c8ea0c151661ce39d36
matsqa <iliunovmat@gmail.com>
2022-02-22 17:44:05 -0800

    add updates for task 1 of module 2

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,21 +3,14 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-//export
-function createGetter(path) {
-  //console.log(this.category);
-    return () => {
-      console.log();
-      //return obj[title];
+export function createGetter(path) {
+  return (obj) => {
+    if (Object.keys(obj).length === 0) {
+      return undefined;
+    } else {
+      return path.split('.').reduce((o, i)=> o[i],  obj);
     }
-}
 
-const product = {
-  category: {
-    title: "Goods"
-  }
-}
+  };
 
-const getter = createGetter('category.title');
-//console.log(product.category.title);
-console.log(getter(product)); // Goods
+}
